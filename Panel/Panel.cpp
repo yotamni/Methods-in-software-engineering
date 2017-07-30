62473410a6dee286df6e45a48a158a29e94a92ef
TerryMeir <terry.lachmani@gmail.com>
2017-07-30 20:33:37 +0300

    panel.cpp push

---
+++ Panel/Panel.cpp
@@ -0,0 +1,20 @@
+#include "Panel.h"
+
+
+
+Panel::Panel()
+{
+}
+
+
+Panel::~Panel()
+{
+}
+
+void Panel::draw(Graphics& g)
+{
+	drawAll(g);
+}
+void Panel::keyDown(int keyCode, char charecter) {
+
+}

98da453bc42c6b5dd1717a3dba688950bf69353f
TerryMeir <terry.lachmani@gmail.com>
2017-08-01 21:18:52 +0300

    button commit

--- Button/Button.h
+++ Button/Button.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "../Label/Label.h"
+
 struct MouseListener
 {
 	virtual void MousePressed(Control &control, int x, int y, bool isLeft) = 0;
 };
+
 class Button :public Label
 {
 	MouseListener* listener;

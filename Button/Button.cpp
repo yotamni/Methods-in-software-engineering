e8708388be43799a78731a2e02c5df49292f1802
Aerocrs\eldad <eldad@aerocrs.com>
2017-08-02 09:15:21 +0300

    Revert "button commit"
    This reverts commit 98da453bc42c6b5dd1717a3dba688950bf69353f.

--- Button/Button.cpp
+++ Button/Button.cpp
@@ -1,8 +1,14 @@
 #include "Button.h"
 
-Button::Button(int width, int hight):Label(width,hight){}
-Button::~Button(){}
 
+Button::Button(int width, int hight):Label(width,hight)
+{
+//	canFocus = true;
+}
+Button::~Button()
+{
+
+}
 void Button::addListener(MouseListener& listener)
 {
 	this->listener = &listener;
--- Button/Button.h
+++ Button/Button.h
@@ -1,11 +1,9 @@
 #pragma once
 #include "../Label/Label.h"
-
 struct MouseListener
 {
 	virtual void MousePressed(Control &control, int x, int y, bool isLeft) = 0;
 };
-
 class Button :public Label
 {
 	MouseListener* listener;
